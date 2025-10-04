b4a21a96bbacb41e2795305e68aaeac7fb82c9f3
cilobar01-source <cilobar01@gmail.com>
2025-10-04 13:42:36 +0700

    Add files via upload

---
+++ roles.js
@@ -0,0 +1,9 @@
+export const ROLES = {
+  ADMIN_RT: "admin_rt",
+  BENDAHARA_RT: "bendahara_rt",
+  ADMIN_PKK: "admin_pkk",
+  BENDAHARA_PKK: "bendahara_pkk",
+  ADMIN_KT: "admin_kt",
+  BENDAHARA_KT: "bendahara_kt",
+  WARGA: "warga",
+};
